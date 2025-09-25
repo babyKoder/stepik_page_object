6f352755cc8c2f5f797d29ac26deda31ca65b78f
Artem Osipov <artmp.omlep@gmail.com>
2025-09-25 23:42:03 +0300

    finall v loginPage

--- test_main_page.py
+++ test_main_page.py
@@ -6,6 +6,12 @@ def test_guest_can_go_to_login_page(browser):
     main_page = MainPage(browser, link)
     main_page.open()
 
+    # вариант 1)
+    # main_page.go_to_login_page()
+    # login_page = LoginPage(browser, browser.current_url)
+    # login_page.should_be_login_page()
+
+    # вариант 2
     login_page = main_page.go_to_login_page()
     login_page.should_be_login_page()
 

2fb90f900aeb25bdb879a1c12be9c02c9b20b6b4
Artem Osipov <artmp.omlep@gmail.com>
2025-09-25 23:46:40 +0300

    finall v loginPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,13 +8,13 @@ def test_guest_can_go_to_login_page(browser):
     main_page.open()
 
     # вариант 1)
-    # main_page.go_to_login_page()
-    # login_page = LoginPage(browser, browser.current_url)
-    # login_page.should_be_login_page()
+    main_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
     # вариант 2
-    login_page = main_page.go_to_login_page()
-    login_page.should_be_login_page()
+    # login_page = main_page.go_to_login_page()
+    # login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):

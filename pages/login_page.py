ce73071b22327bc00e464dc042c0c3a84f7a7cbc
Artem Osipov <artmp.omlep@gmail.com>
2025-09-25 23:33:13 +0300

    fix create loginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,3 @@
-from section1.lesson1_step2_ import browser
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
@@ -10,8 +9,8 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        print(browser.current_url)
-        assert 'accounts7' in browser.current_url, 'Url ot contain a login'
+        # print(self.browser.current_url)
+        assert 'accounts' in self.browser.current_url, 'Url not contain a login'
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -3,10 +3,11 @@ from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    main_page = MainPage(browser, link)
+    main_page.open()
 
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    login_page = main_page.go_to_login_page()
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -14,6 +15,3 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-def tes

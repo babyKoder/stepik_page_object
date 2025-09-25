3b5c3a9576b1ef83c4759c9d0e2bf4ec9a84b539
Artem Osipov <artmp.omlep@gmail.com>
2025-09-25 23:17:13 +0300

    create loginPage

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from section1.lesson1_step2_ import browser
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        print(browser.current_url)
+        assert 'accounts7' in browser.current_url, 'Url ot contain a login'
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration form is not presented"
